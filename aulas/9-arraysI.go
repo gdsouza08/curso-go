bd56d911be3ffc71b11a9f12646018e872fd29e3
Guilherme Amorim <gdsouza.ebt@gmail.com>
2025-09-07 10:20:31 -0300

    declarando valores no array

---
+++ aulas/9-arraysI.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Declarando array de 5 elementos do tipo inteiro
+	var numeros [5] int
+
+	numeros [0] = 10
+	numeros [1] = 20
+	numeros [2] = 30
+	numeros [3] = 40
+	numeros [4] = 50
+
+	fmt.Println("Arrays de", numeros)
+}
